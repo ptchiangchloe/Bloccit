93e19d04948074d91782000b2ff62f34f4f3e79f
Han Jiang <ptchiang12@gmail.com>
2016-02-11 15:13:31 -0800

    correct codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,13 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by(
+  title: "a unique title",
+  body: "a unique body"
+)
+puts "#{Post.count}"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
