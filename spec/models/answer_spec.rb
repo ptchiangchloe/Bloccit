85a67bb047ff17ccc9e083b366e63388ff63eae7
Han Jiang <ptchiang12@gmail.com>
2016-02-11 17:42:03 -0800

    checkpoint 28 revised

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body",
-  resolved: "True or False") }
+  resolved: true) }
    let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
 
    describe "attributes" do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body",
-  resolved: "True or False") }
+  resolved: true) }
 
    describe "attributes" do
      it "responds to title" do
