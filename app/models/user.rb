b0435191256dd3dcfc6dc9bbd5f6bf9ecf9b8b6a
Han Jiang <ptchiang12@gmail.com>
2016-02-23 10:05:18 -0800

    assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase }
+  before_save :format_name
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, length: { minimum: 1,maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum:6 }, if: "password_digest.nil?"
@@ -11,4 +12,16 @@ class User < ActiveRecord::Base
             format: { with: EMAIL_REGEX }
 
   has_secure_password
+
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
+
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
@@ -22,6 +23,12 @@ RSpec.describe User, type: :model do
 
     it "should respond to email" do
       expect(user).to respond_to(:email)
+    end
+
+    it "should format the user's name" do
+      user.name = "bloc user"
+      user.save
+      expect(user.name).to eq "Bloc User"
     end
   end
 
