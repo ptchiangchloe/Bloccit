a0eff2e866ed6db1eb1e2f77219f42790b1d74cb
Han Jiang <ptchiang12@gmail.com>
2016-02-23 16:28:04 -0800

    assignment 37 completed 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def form_group_tag(erorrs, &block)
+  def form_group_tag(errors, &block)
     css_class = 'form-group'
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
