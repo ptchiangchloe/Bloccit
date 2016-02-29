8b9922b05e68f1c9c5f0e617460f310d905584cd
Han Jiang <ptchiang12@gmail.com>
2016-02-29 15:20:17 -0800

    temperary check in

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,44 @@
+class ConmentsController < ApplicationController
+  before_action :require_sign_in
+  before_action :authorize_user, only: [:destroy]
+
+
+  def create
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.new(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      flash[:notice] = "Comment was saved."
+      redirect_to [@post.topic, @post]
+    else
+      flash.now[:alert] = "There was an error saving the comment. Please try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+
+    if comment.destroy
+      flash[:notice] = "Comment was deleted successfully."
+      redirect_to [@post.topic, @post]
+    else
+      flash.now[:alert] = "There was an error deleting the comment."
+      redirect_to [@post.topic, @post]
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def authorize_user
+    comment = Comment.find(params[:id])
+    unless current_user.admin? || current_user == comment.user
+      flash[:alert] = "You do not have permission to delete a comment."
+      redirect_to [comment.post.topic, comment.post]
+    end
+  end
+end
