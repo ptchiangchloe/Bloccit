caf27392757d51b030113a75a4d654bf7a351474
Han Jiang <ptchiang12@gmail.com>
2016-03-02 17:46:28 -0800

    checkpoint 43 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require "random_data"
 
+
 5.times do
   User.create!(
    name:     RandomData.random_name,
@@ -18,13 +19,17 @@ require "random_data"
  topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1,1].sample, user: users.sample)}
 end
+
+
 posts = Post.all
 
 100.times do
@@ -61,3 +66,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
