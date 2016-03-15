2249b4ca0d4b325910d53b8e7c20718095429458
Han Jiang <ptchiang12@gmail.com>
2016-03-14 22:32:19 -0700

    correction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   has_many :commentings
-
+  belongs_to :commentable, polymorphic: true
   belongs_to :user
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
- resources :posts, only: [] do
+ resources :posts do
    resources :comments, module: :posts, only: [:create, :destroy]
  end
 
